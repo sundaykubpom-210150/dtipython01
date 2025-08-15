402a9613e3666babda96c81ed7ba328d61e8546c
sundaykubpom-210150 <sunday0581@gmail.com>
2025-08-15 16:10:03 +0700

    version1 dtipython

---
+++ workshop03.py
@@ -0,0 +1,15 @@
+print('i')
+print("love")
+print('''you''')
+print("""so""")
+
+print(123213131)
+
+print(123123.1231)
+
+print(True)
+print(False)
+
+print(10 + 20)
+print(10 > 20)
+print('Bangna' <= 'Bangpoo')
